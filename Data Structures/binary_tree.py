01bc2e946c9253ee7a524d6f64763a33ad5ad766
Calvonator <calvintate98@gmail.com>
2020-09-08 01:57:09 -0700

    Add traversal functions

--- Data Structures/binary_tree.py
+++ Data Structures/binary_tree.py
@@ -1,7 +1,6 @@
 
 
 
-
 class binary_tree:
     def __init__(self):
         self.root = None
@@ -36,9 +35,35 @@ class binary_tree:
                 parent.right = self.create_node(value)
 
 
+    def pre_order_traversal(self):
+        self.pre_order_traversal_helper(self.root)
+    
+    def pre_order_traversal_helper(self, root):
+        if root != None:
+            print(root.element)
+            self.pre_order_traversal_helper(root.left)
+            self.pre_order_traversal_helper(root.right)
+
+    def in_order_traversal(self):
+        self.in_order_traversal_helper(self.root)
+
+    def in_order_traversal_helper(self, left):
+        if left != None:
+            self.in_order_traversal_helper(left.left)
+            print(left.element)
+            self.in_order_traversal_helper(left.right)
+
+    def post_order_traversal(self):                         #This still needs to be double checked for correctness of results
+        self.post_order_traversal_helper(self.root)
+
+    def post_order_traversal_helper(self, node):
+        if node != None:
+            self.in_order_traversal_helper(node.right)
+            print(node.element)
+            self.in_order_traversal_helper(node.left)
+            
 
         
-
 
 class binary_tree_node:
     def __init__(self, value):
@@ -55,8 +80,16 @@ tree.insert(8)
 tree.insert(7)
 tree.insert(11)
 tree.insert(12)
+tree.insert(4)
+tree.insert(17)
+tree.insert(2)
+tree.insert(6)
+tree.insert(3)
+
 
 root = tree.root
 
-print(root.element)
-print(tree.root.right.element)
+
+#tree.pre_order_traversal()
+#tree.in_order_traversal()
+tree.post_order_traversal()

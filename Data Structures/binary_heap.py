985e3341734b594feb1ca8ce5091517b65bd9682
Calvonator <calvintate98@gmail.com>
2020-09-22 21:03:32 -0700

    Few heap modifications

--- Data Structures/binary_heap.py
+++ Data Structures/binary_heap.py
@@ -2,16 +2,20 @@
 
 class min_heap():
 
-    def __init__(self):
-        self.heap = [0, 5, 9, 11, 14, 18, 19, 21, 33, 17, 27]
-        self.size = 10
+    def __init__(self, *init_heap):
+        self.heap = [0]#, 5, 9, 11, 14, 18, 19, 21, 33, 17, 27]
+        self.size = 0
 
+        if (init_heap):
+            new_heap = []
+            for x in init_heap:
+                new_heap.append(x)
+            self.heapify(new_heap)
 
     def insert(self, value):
         self.heap.append(value)
         self.size += 1
         self.percolate_up()
-
 
     def percolate_up(self):
 
@@ -29,38 +33,80 @@ class min_heap():
 
         return parent, node
 
-    def del_min(self):      
+    def del_min(self):
+
+        del_val = self.heap[1]      
         self.swap(self.size, 1)
         del self.heap[-1]
         self.size -= 1
         self.percolate_down()
 
-    def percolate_down(self):                           #I don't think this is percolating down correctly
+        return del_val
+
+    def percolate_down(self):                          
 
         node = 1
-        child = node * 2
+        mc = self.min_child(node) 
     
-        while self.heap[node] > self.heap[child]:
-            self.swap(node, child)
-            print(self.heap)
-            node, child = self.percolate_up_helper(node, child)
+        while node < self.size:
+            self.swap(node, mc)
+            node = mc
+            mc = self.min_child(node)
 
-    def percolate_down_helper(self, node, child):
-        node = child
-        child = node * 2
+    def min_child(self, parent):
+        
+        left_child = 2 * parent
+        right_child = 2 * parent + 1
+        
+        if right_child > self.size:
+            return left_child
 
-        return node, child
+        if self.heap[left_child] < self.heap[right_child]:
+            return left_child
 
+        else:
+            return right_child
 
     def swap(self, x, y):
         self.heap[x], self.heap[y] = self.heap[y], self.heap[x]
 
 
-tst = min_heap()
-print(tst.heap)
+    def treeify(self):              #Modify to use recursion to properly print out a tree structure
+        
+        try:
+            for x in range(1, len(self.heap[1:]) + 1):
+                print(self.heap[x])
+                print(f'{self.heap[2 * x]} - {self.heap[2 * x + 1]}\n')
 
-tst.insert(8)
-print(tst.heap)
+        except IndexError:
+            print(f'{self.heap[-1]}\n')
 
-tst.del_min()
+    def heapify(self, init_heap):
+        if self.size == 0:
+            i = len(init_heap) // 2
+            self.size = len(init_heap)        
+            self.heap = [0] + init_heap
+
+            while (i > 0):
+                self.percolate_down()
+                i -= 1
+
+        else:
+            print("Can not initialise new heap list with an existing heap")
+    
+    #def merge_heap(self):
+
+init = [5, 9, 11, 14, 18, 19, 21, 33, 17, 27]
+
+tst = min_heap(*init)
 print(tst.heap)
+tst.treeify()
+
+#tst.insert(8)
+#print(tst.heap)
+#tst.treeify()
+#tst.del_min()
+#print(tst.heap)
+#tst.treeify()
+
+
--- Data Structures/tst.py
+++ Data Structures/tst.py
@@ -3,6 +3,10 @@
 x = [0, 1, 2, 3, 4, 5]
 
 
-del x[-1]
+#del x[-1]
 
-print(x)
+print(x[1:])
+
+
+for x in range(2, len(x[2:]) + 2):
+    print(x)

b7fc6d5e61eefbc3833b409df054fe5669a1fa8c
Calvonator <calvintate98@gmail.com>
2020-10-09 01:28:04 -0700

    hash table

---
+++ Data Structures/Hash Tables/linear_probe_hash_table.py
@@ -0,0 +1,28 @@
+
+
+class hash_table():
+
+    def __init__(self, table_size):
+        self.size = 0
+        self.array = [None for _ in range(table_size)]
+
+    def hash_index(self, key):
+        return key % self.size
+
+        
+
+    
+
+
+
+
+
+
+
+ar = [None for _ in range(100)]
+
+print(ar)
+
+
+
+
